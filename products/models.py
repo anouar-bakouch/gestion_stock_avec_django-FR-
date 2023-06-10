ff9d4c7cd94b16708e3731f16a0272804be314d9
anouar <anouar.bakouch@usmba.ac.ma>
2023-06-10 20:35:24 +0100

    added bootstrap

--- products/models.py
+++ products/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Fournisseur(models.Model):
-    nFr = models.IntegerField(max_length=50)
+    nFr = models.IntegerField()
     Societe = models.CharField(max_length=50)
-    tel = models.IntegerField(max_length=14)
+    tel = models.IntegerField()
     email = models.EmailField(max_length=50)
     adresse = models.CharField(max_length=50)
 
 class Devis(models.Model) :
-    nDevis = models.IntegerField(max_length = 50)
+    nDevis = models.IntegerField()
     dateDevis = models.DateField()
     montant = models.FloatField(max_length = 50)
     nFr = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
@@ -21,33 +21,33 @@ class Produit(models.Model) :
     marque = models.CharField(max_length = 50)
 
 class DetailDevis(models.Model) :
-    qteProd = models.IntegerField(max_length = 50)
+    qteProd = models.IntegerField()
     prixProd = models.FloatField(max_length = 50)
     nDevis = models.ForeignKey(Devis, on_delete=models.CASCADE)
     RefProd = models.ForeignKey(Produit, on_delete=models.CASCADE)
 
 class Facture(models.Model) :
-    nFact = models.IntegerField(max_length = 50)
+    nFact = models.IntegerField()
     dateFact = models.DateField()
     montantFact = models.FloatField(max_length = 50)
     nFr = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
 
 class Livraison(models.Model) :
-   nBon = models.IntegerField(max_length = 50)
-   qteLiv = models.IntegerField(max_length = 50)
+   nBon = models.IntegerField()
+   qteLiv = models.IntegerField()
    prixLiv = models.FloatField(max_length = 50)
    RefFact = models.ForeignKey(Facture, on_delete=models.CASCADE)
    RefProd = models.ForeignKey(Produit, on_delete=models.CASCADE)
 
 class Commande(models.Model) :
-    nCom = models.IntegerField(max_length = 50)
+    nCom = models.IntegerField()
     dateCom = models.DateField()
     montantCde = models.FloatField(max_length = 50)
     nFr = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
     refDevis = models.ForeignKey(Devis, on_delete=models.CASCADE)
 
 class DetailCommande(models.Model) :
-    qteCde = models.IntegerField(max_length = 50)
+    qteCde = models.IntegerField()
     prixProd = models.FloatField(max_length = 50)
     nCom = models.ForeignKey(Commande, on_delete=models.CASCADE)
     RefProd = models.ForeignKey(Produit, on_delete=models.CASCADE)
