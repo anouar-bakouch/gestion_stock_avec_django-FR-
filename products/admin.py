60431054be0b8dac8cd75dafcbd4c37fb59f8ff5
anouar <anouar.bakouch@usmba.ac.ma>
2023-06-10 20:03:14 +0100

    updated admin and settings

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .products import Fournisseur, Devis, Produit, DetailDevis, Facture, Livraison, Commande, DetailCommande
+
+admin.site.register(Fournisseur)
+admin.site.register(Devis)
+admin.site.register(Produit)
+admin.site.register(DetailDevis)
+admin.site.register(Facture)
+admin.site.register(Livraison)
+admin.site.register(Commande)
+admin.site.register(DetailCommande)
+
